2dbce43d5052a7a08235daafc424a87980c6e1c0
Silea Horia <146817894+Silea-Horia@users.noreply.github.com>
2025-07-29 14:07:19 +0300

    added db generation script (#2)

---
+++ database/generation.sql
@@ -0,0 +1,8 @@
+DROP DATABASE IF EXISTS "air-assist"
+CREATE DATABASE "air-assist"
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
