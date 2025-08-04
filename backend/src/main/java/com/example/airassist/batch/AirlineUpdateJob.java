e11393e00f9bd8a0165b5b962cf857957a2937df
Teodora Catanas <126598875+titoakapiticutzudegradina@users.noreply.github.com>
2025-08-04 11:07:43 +0300

    airline table + initializer(with dependence added) and temporary unique flight id

---
+++ backend/src/main/java/com/example/airassist/batch/AirlineUpdateJob.java
@@ -0,0 +1,36 @@
+package com.example.airassist.batch;
+
+import com.example.airassist.persistence.dao.AirlineRepository;
+import com.example.airassist.persistence.model.Airline;
+import com.opencsv.bean.CsvToBeanBuilder;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.List;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class AirlineUpdateJob {
+    private final AirlineRepository airlineRepository;
+
+    @PostConstruct
+    public void loadData(){
+        try(Reader reader = new FileReader("../database/airlines_clean.csv")){
+            List<Airline> airlines = new CsvToBeanBuilder<Airline>(reader)
+                    .withType(Airline.class)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build()
+                    .parse();
+            airlineRepository.saveAll(airlines);
+            log.info("Airlines data loaded successfully");
+        }catch (Exception e){
+            log.error("Error loading airlines data: {}", e.getMessage());
+            throw new RuntimeException("Failed to load airlines data", e);
+        }
+    }
+}
---
+++ backend/src/main/java/com/example/airassist/persistence/dao/AirlineRepository.java
@@ -0,0 +1,11 @@
+package com.example.airassist.persistence.dao;
+
+import com.example.airassist.persistence.model.Airline;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AirlineRepository extends JpaRepository<Airline, UUID> {
+}
