7715fd32c8635cbf3384be78a54e4023ac102bfa
Teodora Catanas <126598875+titoakapiticutzudegradina@users.noreply.github.com>
2025-08-04 11:30:18 +0300

    airline service and controller + changed the flight_id to be of type UUID

---
+++ backend/src/main/java/com/example/airassist/controller/AirlineController.java
@@ -0,0 +1,28 @@
+package com.example.airassist.controller;
+
+import com.example.airassist.persistence.model.Airline;
+import com.example.airassist.service.AirlineService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@Validated
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/airlines")
+public class AirlineController {
+    private final AirlineService airlineService;
+
+    @RequestMapping
+    public List<Airline> getAllAirlines() {
+        log.info("Get all airlines request received");
+        List<Airline> airlines = airlineService.getAllAirlines();
+        log.info("Get all airlines response, count {}", airlines.size());
+        return airlines;
+    }
+}
