93de5b03e30eb9768887742d029d7e9e85a51fda
AlexiaVoina <alexiavoina2004@gmail.com>
2025-08-13 14:32:33 +0300

    refactored passenger cases list

--- backend/src/main/java/com/example/airassist/persistence/dao/PassengerRepository.java
+++ backend/src/main/java/com/example/airassist/persistence/dao/PassengerRepository.java
@@ -4,6 +4,8 @@ import com.example.airassist.persistence.model.Passenger;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
+public interface PassengerRepository extends JpaRepository<Passenger, UUID> {
 }
